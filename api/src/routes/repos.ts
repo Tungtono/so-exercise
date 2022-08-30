12c1e16c8eebc768f3df005b983541ae47687c0b
Joe <uehtung@gmail.com>
2022-08-30 15:36:19 -0400

    filter data to return
    + Only return repositories where repository.fork is false.
    + refactor code a bit

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -3,37 +3,43 @@ import { readFile } from 'fs/promises';
 
 export const repos = Router();
 
-const apiUrl: string = 'https://api.github.com/users/silverorange/repos';
-const filePath: string = `${__dirname}/../../data/repos.json`;
-let mergedData: any[] = [];
-
-const fetchApiData = async (url: string) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    mergedData = mergedData.concat(data);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const readJsonFile = async (path: string) => {
-  try {
-    const rawData = await readFile(path, 'utf8');
-    mergedData = mergedData.concat(JSON.parse(rawData));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-fetchApiData(apiUrl);
-readJsonFile(filePath);
-
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json(mergedData);
+  let mergedData: any[] = [];
+  const apiUrl: string = 'https://api.github.com/users/silverorange/repos';
+  const filePath: string = `${__dirname}/../../data/repos.json`;
+
+  const fetchApiData = async (url: string) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // mergedData = mergedData.concat(data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const readJsonFile = async (path: string) => {
+    try {
+      const rawData = await readFile(path, 'utf8');
+      // mergedData = mergedData.concat(JSON.parse(rawData));
+      return JSON.parse(rawData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const apiData = await fetchApiData(apiUrl);
+  const jsonData = await readJsonFile(filePath);
+  
+  mergedData = mergedData.concat(apiData).concat(jsonData);
+
+  const nonForkRepos = mergedData.filter((item) => item.fork === false);
+
+  res.json(nonForkRepos);
 });
