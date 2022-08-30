79e5c3bfe4d57769379181b7d2ec100ef12dd1d9
Joe <uehtung@gmail.com>
2022-08-30 16:22:43 -0400

    sort data
    The list of repositories to be displayed in reverse chronological order by creation date.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,17 +17,21 @@ export function App() {
   }, []);
   console.log(dataState);
 
+  const sortedData = dataState.sort((a, b) =>
+    b.created_at.localeCompare(a.created_at)
+  );
   return (
     <div className="App">
       <header className="App-headre">SO Exercise</header>
       <div>
-        {dataState.map((item) => {
+        {sortedData.map((item) => {
           return (
             <div key={item.id} className="repo">
               <div>Repo Name: {item.name}</div>
               <div>Description: {item.description}</div>
               <button>{item.language}</button>
               <div>Number of forks: {item.forks_count}</div>
+              <div>Created at: {item.created_at}</div>
             </div>
           );
         })}
