6822e49d2c0a3f387f8f9c34ab0eb5c5eb033524
Joe <uehtung@gmail.com>
2022-08-30 15:12:11 -0400

    read file JSON
    Asynchronously read data from JSON file using the fs.readFile method from Node

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,17 +1,33 @@
 import { Router, Request, Response } from 'express';
+import { readFile } from 'fs/promises';
 
 export const repos = Router();
 
-const apiUrl = 'https://api.github.com/users/silverorange/repos';
+const apiUrl: string = 'https://api.github.com/users/silverorange/repos';
+const filePath: string = `${__dirname}/../../data/repos.json`;
 let mergedData: any[] = [];
 
 const fetchApiData = async (url: string) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  mergedData = mergedData.concat(data);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    mergedData = mergedData.concat(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const readJsonFile = async (path: string) => {
+  try {
+    const rawData = await readFile(path, 'utf8');
+    mergedData = mergedData.concat(JSON.parse(rawData));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 fetchApiData(apiUrl);
+readJsonFile(filePath);
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
