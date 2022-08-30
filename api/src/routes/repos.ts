8c2efc17c20b3707a93928c8b3c3fce179059a6a
Joe <uehtung@gmail.com>
2022-08-30 14:56:35 -0400

    fetch data github
    Fetch data from Github API using Node native fetch API.
    Initially installed and tried node-fetch package but got error and haven't found a way to fix it yet, will revisit if we still have time later on

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -2,11 +2,22 @@ import { Router, Request, Response } from 'express';
 
 export const repos = Router();
 
+const apiUrl = 'https://api.github.com/users/silverorange/repos';
+let mergedData: any[] = [];
+
+const fetchApiData = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  mergedData = mergedData.concat(data);
+};
+
+fetchApiData(apiUrl);
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(mergedData);
 });
